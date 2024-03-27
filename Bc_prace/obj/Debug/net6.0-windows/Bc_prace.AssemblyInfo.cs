b785a4219e54fd9692685069c52b04ea1a8bd1d9
Lukii <lukas.janecek.02@gmail.com>
2024-03-27 14:44:23 +0100

    First test

--- Bc_prace/obj/Debug/net6.0-windows/Bc_prace.AssemblyInfo.cs
+++ Bc_prace/obj/Debug/net6.0-windows/Bc_prace.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bc_prace")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e533d27a6b88624af3f316136da31e0bfcf1987")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+602c59335c84c11327586b387d9fc70cd3487933")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bc_prace")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bc_prace")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
