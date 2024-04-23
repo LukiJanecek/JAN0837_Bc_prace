94b249ae2146a797d1f1e0c14a550b3a7e80dbbc
Lukii <lukas.janecek.02@gmail.com>
2024-04-23 14:09:14 +0200

    Project rename

--- Bc_prace/Language.Designer.cs
+++ Bc_prace/Language.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Bc_prace {
+namespace JAN0837_BP {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Bc_prace {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Bc_prace.Language", typeof(Language).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("JAN0837_BP.Language", typeof(Language).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
